5586f770c577e4acf687a1b6d8854d278bce1b87
Mazzziu <maty.farias@hotmail.com>
2018-11-19 11:17:34 -0300

    asd

---
+++ cmake-build-debug/CMakeFiles/nodos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nodos.dir/main.cpp.o"
+  "nodos.pdb"
+  "nodos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nodos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/nodos.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nodos.
+# This may be replaced when dependencies are built.
