59c31e90d283ad9b4bfa04e2c62ee5484c5ba7d7
Mazzziu <maty.farias@hotmail.com>
2018-11-19 14:31:20 -0300

    Funcion de la pila

---
+++ funciones.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mazzziu on 19/11/2018.
+//
+
+#include "funciones.h"
---
+++ funciones.h
@@ -0,0 +1,8 @@
+//
+// Created by Mazzziu on 19/11/2018.
+//
+
+#ifndef NODOS_FUNCIONES_H
+#define NODOS_FUNCIONES_H
+
+#endif //NODOS_FUNCIONES_H
